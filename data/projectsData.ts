c776bc8dc24b5939ab4a593888862af5129fe28f
DevHazel <DevHazel@vitamate.kr>
2023-09-01 11:14:11 +0900

    Add my Project 모행

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,11 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: '모두의 여행',
+    description: `모두의 여행은 국내 여행 커뮤니티 플랫폼입니다
+
+    국내의 다양한 여행지를 검색하여 직접 여행 코스를 작성할 수 있고, 해당 일정을 다른 사용자들과 공유하며 국내 여행을 보다 더 편리하고 즐겁게 할 수 있는 서비스를 목표로 진행한 프로젝트입니다`,
+    imgSrc: '/static/images/모행.png',
+    href: 'https://github.com/HelloHazel/Mohaeng',
   },
 ]
 
