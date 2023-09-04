43aaf90c81588f6406e223e6093bf81ad33bc1a2
DevHazel <DevHazel@vitamate.kr>
2023-09-04 12:47:29 +0900

    Add Posting

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,7 +13,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            {/* 프로젝트에 관한 더 좋은 설명은 없을까? */}
           </p>
         </div>
         <div className="container py-12">
